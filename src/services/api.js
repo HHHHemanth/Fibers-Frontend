8c47ec5c08cd9493e5a0861076f8d39e43fac648
HHHHemanth <hemanth2102003acc2@gmail.com>
2026-04-10 15:15:29 +0530

    updated hosted backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://127.0.0.1:8000";
+// const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://fiber-backend.onrender.com";
 
 export const uploadImage = async (file) => {
   const formData = new FormData();
